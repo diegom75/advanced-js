39dc18203062361960c5dd8ec447f65c02cb443d
Diego Marquez <diegom75@nycstudents.net>
2025-02-04 15:18:53 +0000

    1

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -1,18 +1,64 @@
 // List 4 data types in a multiline comment
 
+/* 
+    Interger
+    String
+    Float
+    Boolean
+*/
+
 // Typeof
+
+console.log(typeof 52);
+console.log(typeof "feed");
+console.log(typeof xd);
+console.log(typeof undefined);
+console.log(typeof true);
+
+var x = 1;
+console.log(typeof x)
 
 // Let vs Const vs Var
 
+var x = 2;
+let age = 15
+const miami = 80;
+
+age = 20;
+age = 30;
+age = 40;
+
 // Reassignment (no let)
+age = 20;
+age = 30;
+age = 40;
 
 // Essential Operations
 
-// Additional operations
+let sum = age + 2;
+console.log(sum);
+sum = sum - 5;
+console.log(sum);
+sum = sum / 2;
+console.log(sum);
+sum = sum + 10;
+sum += 10;
+console.log(sum);
 
+
+// Additional operations
+sum -= 4;
+console.log(sum);
+sum *= 4;
+console.log(sum);
 // User Input
 
+const subject = prompt("what is your favorite subject?");
+console.log("your fav subject is" + subject);
+
 // Concatenation v. templates
+
+
 
 // String Methods (uppercase, lowercase, includes)
 
